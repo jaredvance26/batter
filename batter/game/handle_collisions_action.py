8d5d1ce7f17bafca7810200d8d268dfecbc8fc00
Brandon Densmore <densmoreb@205-185-96-174.directcom.com>
2021-06-09 12:15:12 -0600

    got the objects to show

--- batter/game/handle_collisions_action.py
+++ batter/game/handle_collisions_action.py
@@ -15,11 +15,12 @@ class HandleCollisionsAction(Action):
         Args:
             cast (dict): The game actors {key: tag, value: list}.
         """
-        marquee = cast["marquee"][0] # there's only one
-        robot = cast["robot"][0] # there's only one
-        artifacts = cast["artifact"]
-        marquee.set_text("")
+        #marquee = cast["marquee"][0] # there's only one
+        paddle = cast["paddle"][0] # there's only one
+        brick = cast["brick"]
+        #marquee.set_text("")
+        artifacts = []
         for artifact in artifacts:
-            if robot.get_position().equals(artifact.get_position()):
+            if paddle.get_position().equals(artifact.get_position()):
                 description = artifact.get_description()
-                marquee.set_text(description) 
+                #marquee.set_text(description) 
